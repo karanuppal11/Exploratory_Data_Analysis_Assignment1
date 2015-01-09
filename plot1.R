0ef1f66cc938081f2a53b0a2209034ccf9a9deaf
karanuppal11 <karanuppal11@gmail.com>
2015-01-09 17:49:40 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,4 @@
-## First check whether it has the file in the current dir.
-if (!"load_data.R" %in% list.files()) {
-    setwd("~/Desktop/Online Coursera/Coursera-Exploratory-Data-Analysis/ExData_Plotting1/")
-} 
+ 
 source("load_data.R")
 png(filename = "plot1.png", 
     width = 480, height = 480, 
